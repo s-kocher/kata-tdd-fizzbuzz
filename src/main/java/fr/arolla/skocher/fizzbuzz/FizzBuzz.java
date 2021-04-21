8fd971de13ffc8c3913c89b19c99646691a3982c
Sylvain K <skocher@avb202.com>
2021-04-21 15:55:59 +0200

    test for number 6 - green

--- src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
     public static final String FIZZ = "Fizz";
 
     public static String getValue(int input) {
-        if (input == 3) {
+        if (input == 3 || input == 6) {
             return FIZZ;
         }
         return Integer.toString(input);
