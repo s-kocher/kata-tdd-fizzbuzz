14e1424c4c914f2b890e1eba052b9382a7ca441f
Sylvain K <skocher@avb202.com>
2021-04-21 15:42:36 +0200

    create test for number 1

--- src/test/java/fr/arolla/skocher/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fr/arolla/skocher/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,18 @@
 package fr.arolla.skocher.fizzbuzz;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
 
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void should_return_1_for_number_1() {
+        int input = 1;
+
+        String result = FizzBuzz.getValue(input);
+
+        assertEquals("1", result);
     }
 
 }
