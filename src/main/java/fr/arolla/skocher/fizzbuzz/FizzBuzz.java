ab8423a5e20f31881e64a522dc9a33aa992cc172
Sylvain K <skocher@avb202.com>
2021-04-21 15:48:36 +0200

    test for number 2 - refactor

--- src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
@@ -3,9 +3,6 @@ package fr.arolla.skocher.fizzbuzz;
 public class FizzBuzz {
 
     public static String getValue(int input) {
-        if (input == 2) {
-            return "2";
-        }
-        return "1";
+        return Integer.toString(input);
     }
 }
