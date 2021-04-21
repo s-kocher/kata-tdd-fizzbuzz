f90775deeec51eec5ed0dba1668fccd69c2d601d
Sylvain K <skocher@avb202.com>
2021-04-21 15:53:49 +0200

    test for number 3 - refactor

--- src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,11 @@ package fr.arolla.skocher.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "Fizz";
+
     public static String getValue(int input) {
         if (input == 3) {
-            return "Fizz";
+            return FIZZ;
         }
         return Integer.toString(input);
     }
