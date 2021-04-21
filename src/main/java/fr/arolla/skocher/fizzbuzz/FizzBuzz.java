6412ccd531072e8c4db27e904a3b105f090fb62e
Sylvain K <skocher@avb202.com>
2021-04-21 15:47:54 +0200

    test for number 2 - green

--- src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/arolla/skocher/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,9 @@ package fr.arolla.skocher.fizzbuzz;
 public class FizzBuzz {
 
     public static String getValue(int input) {
+        if (input == 2) {
+            return "2";
+        }
         return "1";
     }
 }
